b6128303a935ea5ddc0659db6f44e56cda9eed40
Grayson <russell.hay@gmail.com>
2025-05-23 11:24:39 -0700

    initial blinky, not working, maybe?

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,79 @@
-//! Template
-
-
 #![no_std]
 #![no_main]
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::i2c::{self, Config};
-use embassy_time::Timer;
-use embedded_hal_1::i2c::I2c;
+use embassy_rp::gpio;
+use embassy_time::{Duration, Timer};
+use gpio::{Level, Output};
 use {defmt_rtt as _, panic_probe as _};
 
+use cyw43_pio::{PioSpi, DEFAULT_CLOCK_DIVIDER};
+use defmt::*;
+use embassy_rp::bind_interrupts;
+use embassy_rp::peripherals::{DMA_CH0, PIO0};
+use embassy_rp::pio::{InterruptHandler, Pio};
+use static_cell::StaticCell;
+use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
+#[embassy_executor::task]
+async fn cyw43_task(
+    runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
+) -> ! {
+    runner.run().await
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    info!("Hello World!");
+async fn main(spawner: Spawner) {
+    // Initialise Peripherals
+    let p = embassy_rp::init(Default::default());
+
+    let fw = include_bytes!("../cyw43.bin");
+    let clm = include_bytes!("../cyw43_clm.bin");
+
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        DEFAULT_CLOCK_DIVIDER,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
+    let state = STATE.init(cyw43::State::new());
+    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
+    unwrap!(spawner.spawn(cyw43_task(runner)));
+
+    // Loop
+    loop {
+        // Log
+        info!("LED On!");
+
+        // Turn LED On
+        control.gpio_set(0, true).await;
+        // Wait 100ms
+        Timer::after(Duration::from_millis(100)).await;
+
+        // Log
+        info!("LED Off!");
+
+        // Turn Led Off
+        control.gpio_set(0, false).await;
+
+        // Wait 100ms
+        Timer::after(Duration::from_millis(100)).await;
+    }
 }
